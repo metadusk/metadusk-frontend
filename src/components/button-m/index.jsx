6ac4f228330fa7406810c5db298b5706ba0fd208
metadusk-Y <metaduskY@gmail.com>
2021-12-02 17:36:56 +0800

    update

--- src/components/button-m/index.jsx
+++ src/components/button-m/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {useActiveWeb3React} from "../../web3";
-import {getScanName} from "../../web3/address";
+import {ChainId, getScanName} from "../../web3/address";
 import {changeNetwork} from "../../web3/connectors";
 
 export default function ButtonM({className, chainId: chainId_, onClick, children}){
   const {chainId} = useActiveWeb3React()
   const onConfirm = () => {
-    if (chainId !== chainId_) {
+    if (chainId !== chainId_ && chainId !== ChainId.LOCALHOST) {
       changeNetwork(chainId_)
     } else {
       onClick()
@@ -14,7 +14,7 @@ export default function ButtonM({className, chainId: chainId_, onClick, children
   }
   return (
     <div className={className} onClick={onConfirm}>
-      {chainId !== chainId_ ? `Switch To ${getScanName(chainId_)}` : children}
+      {chainId !== chainId_ && chainId !== ChainId.LOCALHOST ? `Switch To ${getScanName(chainId_)}` : children}
     </div>
   )
 }
