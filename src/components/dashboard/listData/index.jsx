b4c48d25d74aed1b5818d7e0f728a2a98bf392e6
metadusk-Y <metaduskY@gmail.com>
2021-12-02 17:24:28 +0800

    update

--- src/components/dashboard/listData/index.jsx
+++ src/components/dashboard/listData/index.jsx
@@ -157,8 +157,8 @@ const DashBoardListData = ({
         ) : (
           tabFlag === 'equip' && (
             <p className='no_data'>
-              Coming Soon
-              {/* <FormattedMessage id='dashboard13' /> */}
+              {/*Coming Soon*/}
+               <FormattedMessage id='dashboard13' /> 
             </p>
           )
         )}
