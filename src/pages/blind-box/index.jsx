6af7d5323f97ae5d65495328af0a0ca03d2a17c9
metadusk-Y <metaduskY@gmail.com>
2021-12-23 15:41:00 +0800

    style

--- src/pages/blind-box/index.jsx
+++ src/pages/blind-box/index.jsx
@@ -9,14 +9,16 @@ export default function BlindBox() {
 
   const [tab, setTab] = useState('festival')
   return (
-    <div className="blind-box">
+    <>
       <Header/>
-      <div className="tabs">
-        <div className={cs(tab === 'festival' && 'tab-active')} onClick={() => setTab('festival')}>Festival Box</div>
-        <div className={cs(tab === 'common' && 'tab-active')} onClick={() => setTab('common')}>Common Box</div>
+      <div className="blind-box">
+        <div className="tabs">
+          <div className={cs(tab === 'festival' && 'tab-active')} onClick={() => setTab('festival')}>Festival Box</div>
+          <div className={cs(tab === 'common' && 'tab-active')} onClick={() => setTab('common')}>Common Box</div>
+        </div>
+        {tab === 'common' && <CommonBox/>}
+        {tab === 'festival' && <FestivalBox/>}
       </div>
-      {tab === 'common' && <CommonBox/>}
-      {tab === 'festival' && <FestivalBox/>}
-    </div>
+    </>
   )
 }
